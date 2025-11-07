b58c1ebcee5861d38c4cb04ab7ab2bcf7eb82407
DanyLientsoa <daniel.p13.ceres@gmail.com>
2025-11-07 18:56:16 +0300

    edit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,19 +3,29 @@ import "./App.css";
 import { Button } from "./composants/Button";
 import { Input } from "./composants/Input";
 import { Lock, Mail } from "lucide-react";
+import { Text } from "./composants/Text";
 
 function App() {
-
-   const [email, setEmail] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   return (
     <>
-      <div  className="bg-white rounded-xl p-6">
-        <p className="text-3xl font-bold underline">hello test</p>    
+      <div className="p-6 space-y-4 bg-gray-50 dark:bg-gray-900 ">
+        <Text size="sm">Petit texte</Text>
+        <Text size="md" variant="medium">
+          Texte moyen et medium
+        </Text>
+        <Text size="lg" variant="bold" color="primary">
+          Texte large et bold
+        </Text>
+        <Text size="xl" color="secondary">
+          Texte extra-large, couleur secondaire
+        </Text>
+        <Text color="error">Texte d’erreur</Text>
+        <Text color="success">Texte succès</Text>
       </div>
-        
-     <div className="w-full max-w-sm space-y-4 bg-white dark:bg-gray-800 shadow-lg rounded-2xl p-6">
+      <div className="w-full max-w-sm space-y-4 bg-white dark:bg-gray-800 shadow-lg rounded-2xl p-6">
         <h1 className="text-2xl font-semibold text-gray-800 dark:text-white text-center">
           Connexion
         </h1>
@@ -39,7 +49,6 @@ function App() {
         />
 
         <Button variant="primary">Se Connecter</Button>
-        <Button variant="secondary">Annuler</Button>
       </div>
     </>
   );
