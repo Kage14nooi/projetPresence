7320e5223aed3a1755ab6454a3386819e0e030ed
DanyLientsoa <daniel.p13.ceres@gmail.com>
2025-11-07 17:39:12 +0300

    feat: add reusable input component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,46 @@
+import { useState } from "react";
 import "./App.css";
 import { Button } from "./composants/Button";
+import { Input } from "./composants/Input";
+import { Lock, Mail } from "lucide-react";
 
 function App() {
 
+   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <>
-      <div>
-        <p className="text-3xl font-bold underline">hello word</p>
+      <div  className="bg-white rounded-xl p-6">
+        <p className="text-3xl font-bold underline">hello test</p>    
       </div>
-       <div className="flex flex-rows gap-4 items-center justify-center bg-gray-50">
-        <Button>Default</Button>
-        <Button variant="secondary">Secondaire</Button>
-        <Button variant="outline">Outline</Button>
-        <Button variant="danger">Supprimer</Button>
-        <Button variant="ghost">Ghost</Button>
-        <Button size="lg" variant="primary">
-          Grand Bouton
-        </Button>
-    </div>
+        
+     <div className="w-full max-w-sm space-y-4 bg-white dark:bg-gray-800 shadow-lg rounded-2xl p-6">
+        <h1 className="text-2xl font-semibold text-gray-800 dark:text-white text-center">
+          Connexion
+        </h1>
+
+        <Input
+          label="Adresse e-mail"
+          icon={Mail}
+          type="email"
+          placeholder="exemple@email.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <Input
+          label="Mot de passe"
+          icon={Lock}
+          type="password"
+          placeholder="••••••••"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <Button variant="primary">Se Connecter</Button>
+        <Button variant="secondary">Annuler</Button>
+      </div>
     </>
   );
 }
